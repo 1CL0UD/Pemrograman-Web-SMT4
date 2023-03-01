7aa386ffc4fa931bbadaabd2b744ead27b78e5b3
Shalahuddin-Abdul <94779696+Shalahuddin-Abdul@users.noreply.github.com>
2023-03-01 10:51:45 +0700

    Add files via upload

---
+++ Praktikum/Praktikum2-ShalahuddinAbdulAziz/process-form.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["nama"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?><br>
+Your message is: <?php echo $_GET["pesan"];?>
+
+</body>
+</html>
